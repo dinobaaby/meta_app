dce32e220178a7d8bfdf14a23b033a62cb049936
TrinhHieu <trinhhieu758@gmail.com>
2024-03-21 20:46:19 +0700

    addcustomcolor

---
+++ lib/utils/messenger/ms_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const active_ms_color = Color.fromRGBO(68, 213, 88, 1.0);
+const title_ms_color = Color.fromRGBO(227, 229, 234, 1.0);
+const action_ms_color = Color.fromRGBO(35, 35, 35, 1.0);
+const ms_background_color = Color.fromRGBO(0, 0, 1, 1.0);
+const chatdesc_ms_color = Color.fromRGBO(35, 35, 35, 1.0);
+const action_appbar_ms_color = Color.fromRGBO(37, 117, 186, 1.0);
